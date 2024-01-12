6017ffd04659bbf5a69bc64708881dee4f53f793
Doyoonasdf <won1hana@naver.com>
2024-01-12 16:49:23 +0900

    2024012_전체적으로 복습 및 정리

--- js/main.js
+++ js/main.js
@@ -6,26 +6,28 @@ const btnPlay = document.querySelector('.play');
 const btnPause = document.querySelector('.pause');
 const showNum = 3; //전체 패널갯수는 5개지만 화면에 보이는 패널은 3개니깐
 const speed = 500;
-const interval = 3000;
+const interval = 3000; //자동으로 전환되는 간격
 const len = list.children.length; //전체 패널의 갯수
 let enableClick = true; //재이벤트방지구문
 //버튼클릭할때 모션중 재이벤트방지해야함 왜? 일반 css이용해서 append prepend이용해서 싸스로만 동작이되는거면 굳이 재이벤트방지 안해도 흐트러지는게 없는데 좌우버튼 클릭할때마다 css가 아닌 requestAnimationFrame으로 만든  anime.js를 이용해서 동작할거기때문에 광클하면 모션이 엉킴
-let currentNum = 0; //현재 활성화 된 패널
+let currentNum = 0; //현재 활성화 된 패널의 인덱스를 나타냄
 
 let timer = null; //전역으로 필요한 이유 : setInterval에 timer 값을 넣어서 clearInterval로 자동정지 롤링풀수있게
 
 init();
 //패널이 양옆으로 배치되도록 처리
 
-setTimeout(startRolling, interval);
+setTimeout(startRolling, interval); //setTimeout는 특정 작업이 나중에 실행되도록 예약할 때 사용
 //startRolling(); <- 이렇게 하면 처음 로딩됐을때 바로 2번이 넘어가서 보기 안좋으니깐 스타트롤링자체를 Interval 간격뒤에 실행이 되도록 해야함
 
+//클릭 이벤트가 발생했을 때, 콜백 함수(() => { next(); })가 실행 콜백 함수 내에서 next() 함수가 호출되므로, 실제 클릭 이벤트가 발생할 때 next 함수가 실행됩니다.
 btnNext.addEventListener('click', () => {
 	next();
 	stopRolling(); //좌우버튼을 클릭할때도 정지가 되어야하니깐
 });
 
-// btnNext.addEventListener('click', next());
+// btnNext.addEventListener('click', next()); // next() 함수가 즉시 호출되고, 그 반환값이 이벤트 리스너로 전달됨 실제로 클릭 이벤트가 발생할 때는 아무런 일도 일어나지 않습니다. 왜냐하면 next() 함수가 이미 즉시 호출되어 버렸기 때문
+
 btnPrev.addEventListener('click', () => {
 	prev();
 	stopRolling(); //좌우버튼을 클릭할때도 정지가 되어야하니깐
@@ -34,31 +36,33 @@ btnPrev.addEventListener('click', () => {
 
 btnPlay.addEventListener('click', startRolling); //인수 전달할게없으니깐 바로 클릭하면 바로 호출되게
 btnPause.addEventListener('click', stopRolling);
+
 function init() {
 	list.style.left = -100 / showNum + '%';
 	list.prepend(list.lastElementChild);
 }
 //현재활성화되어있는 패널을 기점으로 다음 활성화될 패널 순번을 구한다.
+//클릭을 하면은 enableClick이 false
 function next() {
 	if (!enableClick) return; //트루가 아니면 모션중인거니깐 리턴
-	enableClick = false;
+	enableClick = false; //이미지 전환 중에는 클릭을 비활성화해야하니깐
 
+	//다음 이미지 인덱스 계산 및 현재 이미지 인덱스 업데이트
 	let nextNum = null;
 	currentNum !== len - 1 ? (nextNum = currentNum + 1) : (nextNum = 0);
 	//만약에 커렌트넘값이 마지막 순번값이 아니면은 더 증가할게 있는거니깐 current_num에서 1을더함 그런게아니면은 넥스트넘은 마지막이여서 할게없는거니깐 처음순번값인 0으로 초기화한다.
-
 	currentNum = nextNum; //이제 순번이 바꼈으니깐 nextNum값이 currentNum값이되야함
 
 	new Anim(list, {
 		//그와동시에 anime로 list의 위치값을 바꿔준다.
 		prop: 'left',
 		value: (-100 / showNum) * 2 + '%',
-		// 현재 위치값이 (-100/showNum) *1 만큼 마이너스 간 상태니깐 그 상태에서 한 번 더 앞으로 가려면은 *2
+		// -100/showNum은 한 번에 이미지 하나의 크기만큼 왼쪽으로 이동하는 비율이고 *2를 하는이유는 다음 이미지가 현재 보이는 이미지의 크기만큼 완전히 슬라이딩 되어야함
 		duration: speed,
 		callback: () => {
-			//모든 모션이 끝나면 초기화 시켜줘야해서 콜백으로 처리
-			list.append(list.firstElementChild);
-			list.style.left = -100 / showNum + '%';
+			//애니메이션이 완료된 후에 실행되는걸 정의
+			list.append(list.firstElementChild); //현재 화면에는 [1, 2, 3] 실행되면 1 번째 이미지가 맨 뒤로 이동하여 이미지 목록은 [2, 3, 1] 됨 .
+			list.style.left = -100 / showNum + '%'; // 이미지 목록을 한 번에 한 이미지씩 왼쪽으로 이동시키면서, 다시 초기 위치로 되돌리는 역할 그 결과, 화면에는 [2, 3, 1]의 이미지가 보임
 			enableClick = true; // enableClick(true) 라고 하면 동작안됨
 		},
 	});
@@ -69,15 +73,15 @@ function prev() {
 	enableClick = false;
 	let prevNum = null;
 	currentNum !== 0 ? (prevNum = currentNum - 1) : (prevNum = len - 1);
-	//현재넘이 0이아니면 받을게 있는거니깐 기존의 커렌트넘에서 1을빼야함
+	// 현재 이미지의 인덱스가 0이 아니라면 이전 이미지의 인덱스를 currentNum - 1로 설정하고, 현재 이미지의 인덱스가 0이면 마지막 이미지의 인덱스로 설정
 	currentNum = prevNum;
 
 	new Anim(list, {
 		prop: 'left',
-		value: (-100 / showNum) * 0 + '%', // *0 ? 0%로 가면되니깐
+		value: (-100 / showNum) * 0 + '%', //  이동 크기를 0으로 설정하여 이미지를 초기 위치로 되돌리는 역할로 이미지를 원래 위치로 되돌려서 이전 이미지가 다시 보이게끔 하는 것  즉 현재가 [1,2,3] 이면 실행하면 [3,1,2]
 		duration: speed,
 		callback: () => {
-			list.prepend(list.lastElementChild);
+			list.prepend(list.lastElementChild); // [1, 2, 3]에서 실행하면 [3, 1, 2]
 			list.style.left = -100 / showNum + '%';
 			enableClick = true;
 		},
@@ -96,13 +100,16 @@ function activation(index) {
 //play와 pause버튼을 눌렀을때 자동으로 롤링시작/정지하는 기능
 function startRolling() {
 	next(); //스타트롤링이 시작되면 자동으로 한 번 넘어가고
-	timer = setInterval(next, interval); //	 그 후에 interval간격대로 넘어가게
+
+	//setInterval은 일정한 시간 간격마다 지정된 함수를 반복적으로 실행
+	timer = setInterval(next, interval); //	 그 후에 next함수가  interval 시간간격대로 넘어가게
 	//로딩바가 그어지는 @keyframes ani 는 2초로 했는데, 여기서는 3초로하는이유? 3초가 정밀하지 않아서 3초로 걸어도 실제로는 2.5초정도임 선이 빨리 그어지는게 중요하니깐 2초 그 후에 작업되도록
 	btnPlay.classList.add('on'); //스타트롤링이 시작되면 자동적으로 재생버튼 활성화되도록
 	btnPause.classList.remove('on');
 }
 
 function stopRolling() {
+	//clearInterval는 setInterval 함수에 의해 설정된 타이머를 종료하고 더 이상 함수가 반복적으로 호출되지 않도록
 	clearInterval(timer); //자동롤링 끊어줌
 	btnPause.classList.add('on');
 	btnPlay.classList.remove('on');
